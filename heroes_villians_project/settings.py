998ae85481d58e11a462fa92de04a1c6b1227599
Chase Coker <Ckcoker90@gmail.com>
2022-04-18 13:21:15 -0500

    Staged Secret Key In Local settings + Properly Formatted Databases

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b7g5c_)l@^r0+63mt6fn-urc+o2-ezf77o^1$+-l^u!5)xr1$a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -68,20 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'heroes_villians_project',
-        'HOST':'localhost',
-        'USER':'root',
-        'PASSWORD': 'Password2022'
-    }
-}
 
 
 # Password validation
